633d17547b2dae6d68693b22a1740a82ced4f0b6
Henrique dalla cort <henriquedallacort@hotmail.com>
2022-05-18 23:09:08 -0300

    Post pelo navegador

---
+++ src/main/java/br/espm/cambio/Moeda.java
@@ -0,0 +1,32 @@
+package br.espm.cambio;
+
+public class Moeda {
+    
+    private String nome;
+    private String simbolo;
+
+    public Moeda(){
+    }
+
+    public Moeda(String nome, String simbolo){
+        this.nome = nome;
+        this.simbolo = simbolo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getSimbolo() {
+        return simbolo;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setSimbolo(String simbolo) {
+        this.simbolo = simbolo;
+    }
+
+}
